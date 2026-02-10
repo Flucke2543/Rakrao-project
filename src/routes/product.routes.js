c4a8da464986a550e1086a7670af09582768780a
Flucke2543 <Poonawitmuenwat@gmail.com>
2026-02-10 16:51:42 +0700

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const productController =
+  require('../modules/products/product.controller');
+
+// ดูสินค้าทั้งหมด
+router.get('/', productController.getProducts);
+
+// เพิ่มสินค้า
+router.post('/', productController.createProduct);
+
+// แก้สินค้า
+router.put('/:id', productController.updateProduct);
+
+// ลบสินค้า
+router.delete('/:id', productController.deleteProduct);
+
+module.exports = router;
 
